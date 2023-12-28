4937cfffdb4bbaa045bd22d60bb4d4c710a21797
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-12-28 22:08:14 +0530

    Changed photo

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
+import { Typewriter } from "react-simple-typewriter";
 
 export const Intro = () => {
     return (
-        <div className="sm:mx-6 md:mx-12 mt-[4rem] flex flex-row justify-center">
+        <div className="sm:mx-6 md:mx-16 mt-[4rem] text-white flex flex-row justify-center">
             <div className="flex flex-col w-[100%]">
                 <div
                     className="text-white md:text-[2rem] lg:text-[4rem] w-[100%]"
                     style={{ fontFamily: 'Marcellus SC, serif' }}
                 >
                     Hello
-                </div>;
+                </div>
                 <div
                     className="text-white md:text-[2rem] lg:text-[4rem] w-[100%]"
                     style={{ fontFamily: 'Marcellus SC, serif' }}
                 >
                     I'm Swapnil Pant
-                </div>;
+                </div>
                 <div
                     className="text-white md:text-[2rem] lg:text-[4rem] w-[100%]"
                     style={{ fontFamily: 'Marcellus SC, serif' }}
                 >
-                A Full Stack Developer
+                    <Typewriter words = {["A Full Stack Developer", "A Software Engineer"]} />
                 </div>
             </div>
 
-            <img src="./swapnil_anim.jpeg" className="sm:w-[7rem] md:w-[20rem] lg:w-[25rem]"/>
+            <img src="./swapnil_pant.png" className="sm:w-[7rem] md:w-[14rem] lg:w-[22rem]"/>
         </div>
     )
 }
