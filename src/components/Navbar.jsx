816d5860153dc7bb405fcd10aad93a10fac10b1d
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-12-28 16:10:37 +0530

    Created navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Navbar = () => {
+  return (
+    <div className="w-full bg-[#7B0000] flex flex-row sm:h-[4rem] md:h-[5rem] lg:h-[6rem] items-center justify-end">
+      <img
+        src="./swapnil.png"
+        alt="Swapnil Pant"
+        className="
+            sm:w-[12rem]
+            md:w-[15rem]
+            lg:w-[20rem] 
+            mx-6"
+      />
+    </div>
+  );
+};
